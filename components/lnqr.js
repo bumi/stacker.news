c63072625ca2053da76d4a9d8bad0b9088469ef9
Michael Bumann <hello@michaelbumann.com>
2021-09-06 17:00:46 +0200

    Pseudo WebLN code

--- components/lnqr.js
+++ components/lnqr.js
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react'
+import { requestProvider } from 'webln';
 import QRCode from 'qrcode.react'
 import { CopyInput, InputSkeleton } from './form'
 import InvoiceStatus from './invoice-status'
 
 export default function LnQR ({ value, statusVariant, status }) {
+  const [useWebln, setUseWebln] = useState(true);
   const qrValue = 'lightning:' + value.toUpperCase()
+
+  useEffect(() => {
+    requestProvider().then(webln => {
+      webln.sendPayment(value).catch(e => {
+        setUseWebln(false);
+      });
+    }).catch(e => {
+      setUseWebln(false);
+    });
+  }, []);
 
   return (
     <>
-      <a className='d-block' href={qrValue}>
-        <QRCode className='h-auto mw-100' value={qrValue} renderAs='svg' size={300} />
-      </a>
-      <div className='mt-3 w-100'>
-        <CopyInput type='text' placeholder={value} readOnly />
-      </div>
+      {!useWebln && (
+        <>
+          <a className='d-block' href={qrValue}>
+            <QRCode className='h-auto mw-100' value={qrValue} renderAs='svg' size={300} />
+          </a>
+          <div className='mt-3 w-100'>
+            <CopyInput type='text' placeholder={value} readOnly />
+          </div>
+        </>
+      )}
       <InvoiceStatus variant={statusVariant} status={status} />
     </>
   )
